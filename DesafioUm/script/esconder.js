6e3cfefd8ebf0646776b3ae4899e4f4306bd5618
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2021-11-20 12:02:24 -0300

    Construção da home e atribuição das imagens

---
+++ DesafioUm/script/esconder.js
@@ -0,0 +1,10 @@
+
+function Esconder(){
+    let esconder = document.getElementById("esconder")
+    // esconder.style.color = "red"
+    if( esconder.style.display != "none"){
+            esconder.style.display = "none"
+    }else{
+        esconder.style.display = "block"
+    }
+}
